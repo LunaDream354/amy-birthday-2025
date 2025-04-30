7c41a70427ab03e3793b9c916aeb8759f971f22e
Luna <luna.g.ribeiro@gmail.com>
2025-04-29 21:03:36 -0300

    added sound

--- randomware/script.js
+++ randomware/script.js
@@ -2,21 +2,45 @@
     const event_open = new Event('open')
     const event_close = new Event('close')
     const section = document.querySelector('#window_randomware')
-
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    
     class Tool{
-        constructor(icon,icon_used,sprite,x,y){
+        constructor(icon,icon_used,sprite,x,y, audio_files){
             this.icon = icon
             this.icon_used = icon_used
             this.sprite = sprite
             this.x = x
             this.y = y
+            this.audio = []
+            async function load_sounds() {
+                for(const url of audio_files){
+                    const response = await fetch(url);
+                    const arrayBuffer = await response.arrayBuffer();
+                    this.audio.add(await audioContext.decodeAudioData(arrayBuffer));
+                }
+                console.log ("Audio from tool loaded")
+            }
+            load_sounds()
+            function playSound(){
+                const audio_chosen = Math.floor(Math.random()*0.99*this.audio.length)
+                const pitch = Math.random()*(0.1+0.1)-0.1
+                const source = audioContext.createBufferSource()
+                source.buffer = this.audio[audio_chosen]
+                source.playbackRate.value = 1.0 + pitch
+                source.connect(audioContext.destination)
+                source.start()
+            }
         }
     }
     const tools = [
-        new Tool('randomware/resource/hammer_icon.png',
-            'randomware/resource/hammer_icon_used.png',
-            'randomware/resource/hammer_crack.png',
-            20,120),
+        new Tool('randomware/resource/hammer/cursor.png',
+            'randomware/resource/hammer/icon_used.png',
+            'randomware/resource/hammer/effect.png',
+            20,120,[
+                'randomware/resource/hammer/sounds/effect_1.mp3',
+                'randomware/resource/hammer/sounds/effect_2.mp3',
+                'randomware/resource/hammer/sounds/effect_3.mp3',
+            ]),
         new Tool('b','b','b'),
         new Tool('c','c','c'),
         new Tool('d','d','d'),
@@ -56,6 +80,7 @@
             const relativeY = mouseY - rect.top;
             effect.style.left = `${relativeX - effect.offsetWidth / 2}px`
             effect.style.top = `${relativeY - effect.offsetHeight / 2}px`
+            current_tool.playSound()
             setTimeout(()=>{
                 section.removeChild(effect)}, 5000)
             setTimeout(()=>{set_cursor(current_tool.icon,current_tool.x,current_tool.y)},500)
